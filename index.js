660a0494678863ad3db884f8f877aba1fb658c6d
Soulmaers <certiss@ya.ru>
2022-11-01 08:22:51 +0300

    !

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ import express from 'express';
 import { PORT } from './config.js'
 import { router as books } from './routes/books.js'
 import { router as user } from './routes/user.js'
+import { router as main } from './routes/index.js'
 import { logger } from './middleware/logger.js'
 import { error as error404 } from './middleware/error.js'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'eis')
+app.use('/load', express.static(__dirname + '/load'))
+
+
 app.use(logger);
 
+
+app.use('/', main);
 app.use('/api/books', books);
 app.use('/api/user', user);
 
